9b6c56a0543019659eb7b9dae67ddbfa349c9551
jfcross4 <jfcross4@gmail.com>
2021-04-22 14:15:55 -0400

    adding notes

---
+++ notes_042221.R
@@ -0,0 +1,11 @@
+# brooklyn numbers
+
+# white (alone), black, hispanic, asian, other
+
+brooklyn <- c(0.368, 0.338, 0.189, 0.127, 1-0.368-0.338-0.189-0.127)
+brooklyn
+
+# find squared differences from Brooklyn (school) average for each school
+# most diverse (or reflective) = schools average the least squared differences
+
+# task: find demographics for Brooklyn Schools
